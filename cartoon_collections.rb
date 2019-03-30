c5e6d100ba8ab68305803bfbc32c310ba0cfa0e3
Kesha C Lake <keshalakemusic@gmail.com>
2019-03-30 01:47:50 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,25 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each.with_index(1) do |dwarf, index|
+    puts "#{index}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.collect do |elements|
+    elements.capitalize!
+    elements << "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any? do |calls|
+    calls.length > 4
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(array)
+    array.find do |cheese|
+      cheese == "cheddar" || cheese == "gouda" || cheese == "camembert"
+    end
+  # cheese_types = ["cheddar", "gouda", "camembert"]
 end
