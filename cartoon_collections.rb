203444d84c3301188884a8ae11a9f961eba43f0f
Kesha C Lake <keshalakemusic@gmail.com>
2019-03-30 01:21:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,8 +4,11 @@ def roll_call_dwarves(array)
   end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.collect do |elements|
+    elements.capitalize
+    elements << "!"
+  end
 end
 
 def long_planeteer_calls# code an argument here
