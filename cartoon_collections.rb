260cf85e4837d2dc3457e9ba00a1d171084d8e07
Kesha C Lake <keshalakemusic@gmail.com>
2019-03-30 01:47:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,9 @@ def long_planeteer_calls(array)
   end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(array)
+    array.find do |cheese|
+      cheese == "cheddar" || cheese == "gouda" || cheese == "camembert"
+    end
+  # cheese_types = ["cheddar", "gouda", "camembert"]
 end
